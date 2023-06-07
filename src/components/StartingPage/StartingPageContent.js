a925aa269192161142b19ee129190468a23acb1e
Weerawi <weerawikramatharindu@gmail.com>
2023-06-07 13:07:36 +0530

    react-typed install

--- src/components/StartingPage/StartingPageContent.js
+++ src/components/StartingPage/StartingPageContent.js
@@ -1,9 +1,22 @@
 import classes from './StartingPageContent.module.css';
+import Typed from 'react-typed';
+
 
 const StartingPageContent = () => {
   return (
     <section className={classes.starting}>
-      <h1>Welcome on Board!</h1>
+      <h2>Welcome on Board!</h2>
+      <Typed
+                strings={[
+                    'Search for products',
+                    'Search for categories',
+                    'Search for brands']}
+                    typeSpeed={40}
+                    backSpeed={50}
+                    attr="placeholder"
+                    loop >
+                    <input type="text"/>
+                </Typed>
     </section>
   );
 };
