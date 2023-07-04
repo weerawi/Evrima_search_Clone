8c6ba045a530dbf8ed4cc6ffd66c39e332e0c4bd
Weerawi <weerawikramatharindu@gmail.com>
2023-07-04 22:42:46 +0530

    use auth details to visible navigation components

--- src/components/Layout/MainNavigation.js
+++ src/components/Layout/MainNavigation.js
@@ -1,23 +1,51 @@
-import { Link } from 'react-router-dom';
- 
+import { Link, useHistory } from 'react-router-dom';
+import { useContext } from 'react'; 
+import AuthContext from '../../store/auth-context';
 
 const MainNavigation = () => {
+
+  const history = useHistory();
+  const authCtx = useContext(AuthContext);
+  const isLoggedIn = authCtx.isLoggedIn;
+  const handleLogout = () => {
+    authCtx.logout();
+    history.replace('/auth'); // Redirect to the login page after logout
+  };
+
+
   return (
-    <header className=" w-full h-24 bg-gray-800 flex justify-between items-center">
+    <header style={{backdropFilter: 'blur(4px)', background: 'rgba(139, 172, 170 , 0.7)',  borderBottom: '1px solid rgba(255, 255, 255, 0.45)'} } className=" w-full h-24  flex justify-between items-center">
       <Link to='/'>
         <div className=" ml-2 text-3xl text-white m-0">Evrima</div>
       </Link>
       <nav>
-        <ul className='m-0 p-0 text-red-700 flex items-baseline hover:text-red-900 font-medium'>
-          <li className='mx-4'>
-            <Link to='/auth'>Login</Link>
-          </li>
-          <li className='mx-4'>
-            <Link to='/profile'>Profile</Link>
-          </li>
-          <li className='mx-4'>
-            <button className='bg-transparent text-red-700 font-bold rounded-md cursor-pointer hover:bg-red-900 p-2 hover:text-red-200'>Logout</button>
-          </li>
+        <ul className='m-0 p-0 md:text-lg  text-red-700 flex items-baseline  font-medium'>
+          {!isLoggedIn && (
+            <li className='mx-4 hover:text-red-900'>
+              <Link to='/auth'>Login</Link>
+            </li>
+          )}
+          {isLoggedIn && (
+            <li className='mx-4 hover:text-red-900'>
+              <Link to='/about'>About</Link>
+            </li>
+          )}
+          {isLoggedIn && (
+            <li className='mx-4 hover:text-red-900'>
+              <Link to='/profile'>Profile</Link>
+            </li>
+          )}
+          
+          {isLoggedIn && (
+            <li className='mx-4 hover:text-red-900'>
+              <Link to='/contact'>Contact</Link>
+            </li>
+          )}
+          {isLoggedIn && (
+            <li className='mx-4'>
+              <button className='bg-transparent text-red-700 font-bold rounded-md cursor-pointer hover:bg-red-900 p-2 hover:text-red-200' onClick={handleLogout}>Logout</button>
+            </li>
+          )}  
         </ul>
       </nav>
     </header>
