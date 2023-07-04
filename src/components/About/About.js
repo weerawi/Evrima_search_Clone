6f0c7909afd2cd40365d4b129687a726c6ba23da
Weerawi <weerawikramatharindu@gmail.com>
2023-07-04 22:47:00 +0530

    created dummy about page

---
+++ src/components/About/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+        <div className="container mx-auto px-4 flex items-center justify-center h-full">
+        <p className="text-white">About </p>
+      </div>
+
+    </div>
+  )
+}
+
+export default About
