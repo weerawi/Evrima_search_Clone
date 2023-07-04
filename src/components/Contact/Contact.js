0e23fa7b5dcde090620b880bb688e8f376b11775
Weerawi <weerawikramatharindu@gmail.com>
2023-07-04 22:47:22 +0530

    created dummy contact page

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>
+        <div className="container mx-auto px-4 flex items-center justify-center h-full">
+        <p className="text-white">CONTACT </p>
+      </div>
+    </div>
+  )
+}
+
+export default Contact
