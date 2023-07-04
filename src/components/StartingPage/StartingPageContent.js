dc9cbbfabaa1d4eb409039124cef9e55d7fe11b5
Weerawi <weerawikramatharindu@gmail.com>
2023-07-04 22:46:22 +0530

    updating strting page

--- src/components/StartingPage/StartingPageContent.js
+++ src/components/StartingPage/StartingPageContent.js
@@ -1,8 +1,7 @@
  
 import Typed from 'react-typed';
 import Card from '../Card/Card';
-import { useState } from 'react';
-import './StartPage.css';
+import { useState } from 'react'; 
 import AOS from "aos";
 import "aos/dist/aos.css";
 AOS.init();
@@ -25,15 +24,42 @@ AOS.init();
   };
 
   return (
-    <div className="overflow-x-hidden h-auto pb-20 flex-row pt-44 items-center justify-center">
-      <div className="mx-auto w-10/12 lg:w-90 mb-56 rounded-md shadow-md p-10 text-center flex-row justify-center items-center my-5 mx-5 bg-gray-200">
+    <div className="overflow-x-hidden h-auto pb-20 flex-row pt-44 items-center   justify-center">
+
+
+
+      <div style={{height:'40vh'}} className="mx-auto w-10/12 lg:w-90 mb-56  p-10 text-center   justify-center items-center my-5 mx-5  ">
+        <h2 className="  text-4xl md:text-6xl text-white ">Welcome to Evrima!</h2>
+        <div style={{color:'white'}}  >
+          <Typed 
+            strings={[
+              '  Search for products',
+              '  Search for categories',
+              '  Search for brands',
+            ]}
+            typeSpeed={40}
+            backSpeed={50}
+            attr="placeholder"
+            loop
+          >
+            <input
+              className="ml-2 bg-transparent text-white text-xl md:text-2xl"
+              type="text"  
+              
+            />
+          </Typed>
+          </div>
+        
+      </div>
+
+      
+
+      <div  style={{backgroundColor:'rgba(0, 0, 0, 0.5)',height:'20vh'}}   className="mx-auto w-10/12 lg:w-90 mb-56 rounded-xl shadow-md p-10 text-center flex-row justify-center items-center my-5 mx-5 bg-trasparent">
         {/* Search Input */}
-        <h2 className="my-8 text-3xl md:text-5xl text-red-800 ">Welcome to Evrima!</h2>
+        
         <Typed
           strings={[
-            '  Search for products',
-            '  Search for categories',
-            '  Search for brands',
+            '  Search Product Here', 
           ]}
           typeSpeed={40}
           backSpeed={50}
@@ -41,7 +67,7 @@ AOS.init();
           loop
         >
           <input
-            className="ml-2 bg-transparent text-gray-800 text-xl md:text-2xl"
+            className="ml-2 bg-transparent text-white text-xl md:text-2xl"
             type="text"
             value={props.value}
             onChange={props.change}
@@ -60,7 +86,7 @@ AOS.init();
         </button>
 
         {/* Filtered Product Cards */}
-        <div className="bg-white flex flex-col border border-gray-300">
+        <div className="bg-gray-300 flex flex-col border border-gray-300">
           {props.result
             .filter((product) => {
               const searchTerm = typeof props.value === 'string' ? props.value.toLowerCase() : '';
